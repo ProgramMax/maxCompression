d49c4bfd3baf9cdd52983fc6fb067ef0dbe63df5
Chris Blume <ProgramMax@gmail.com>
2026-03-06 09:47:36 -0500

    Rename LZ77 to LempelZiv

--- Code/NaiveLempelZiv.hpp
+++ Code/NaiveLempelZiv.hpp
@@ -26,7 +26,7 @@ namespace maxCompression {
 	// So expose the series of spans and references for the different forms to use in their own way.
 	using Segment = std::variant<std::span<uint8_t>, DistanceAndLength>;
 
-	std::vector<Segment> LZ77Compress(const std::span<uint8_t>& input_buffer, uint16_t sliding_window_size, uint8_t minimum_match_length) noexcept;
+	std::vector<Segment> LempelZivCompress(const std::span<uint8_t>& input_buffer, uint16_t sliding_window_size, uint8_t minimum_match_length) noexcept;
 
 }
 
