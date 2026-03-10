6ebe6b43254a6872cc838550f9992dafd1a2be23
Chris Blume <ProgramMax@gmail.com>
2026-03-09 21:24:09 -0400

    Add NaiveDeflate.cpp/.hpp

---
+++ Code/NaiveDeflate.hpp
@@ -0,0 +1,19 @@
+// Copyright 2026, The maxCompression Contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+// This is not a good implementation. Don't use it.
+// The intent is to be the naive, obvious approach.
+// It can then be used as a baseline to verify better, more clever implementations.
+// It can verify that those implementations produce correct results and outperform the naive approach.
+
+
+#ifndef MAXCOMPRESSION_NAIVEDEFLATE_HPP
+#define MAXCOMPRESSION_NAIVEDEFLATE_HPP
+
+namespace maxCompression {
+
+} // namespace maxCompression
+
+#endif // #ifndef MAXCOMPRESSION_NAIVEDEFLATE_HPP
